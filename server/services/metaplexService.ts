370bc10a732bfdf5a598e405f483cd40c4154c46
studerdude <Wyatt.tude@gmail.com>
2025-05-14 12:22:38 -0500

    added privy auth dependency

--- server/services/metaplexService.ts
+++ server/services/metaplexService.ts
@@ -2,4 +2,4 @@ import { createUmi } from '@metaplex-foundation/umi-bundle-defaults'
 import { mplTokenMetadata } from '@metaplex-foundation/mpl-token-metadata'
 
 //RPC endpoint         TODO: replace with mainnet when done testing 
-const umi = createUmi('http://127.0.0.1:8899').use(mplTokenMetadata()) //Testing on localnet
+const umi = createUmi('https://api.devnet.solana.com').use(mplTokenMetadata()) //Testing on devnet
