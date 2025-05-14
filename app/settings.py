d42f02ea752d76273f6a6850fc2526bddc8776c0
Credence Host Computer <58599121+studerdude@users.noreply.github.com>
2025-05-13 23:25:09 -0700

    added credentials to database acceser

--- app/settings.py
+++ app/settings.py
@@ -3,4 +3,4 @@ import os
 
 # Database connection string
 # Update the MYSQL_URL variable with your new credentials
-MYSQL_URL = "mysql+aiomysql://credence_user:choose_secure_password@localhost:3306/credence"
+MYSQL_URL = "mysql+aiomysql://CredenceBackend:c3LVFjPmn5pjjNS1@localhost:3306/credence"
