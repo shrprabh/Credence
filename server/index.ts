20cedcac75c07e8fc95a3132f7c2a12fa674ec8e
studerdude <Wyatt.tude@gmail.com>
2025-06-17 20:46:31 -0500

    fix: added missing files

--- server/index.ts
+++ server/index.ts
@@ -1,8 +1,13 @@
 import express from 'express';
+import mintRoutes from './routes/mintroutes';
 
 const app = express();
 const PORT = 3000;
-const IP = '172.29.213.171';
+const IP = 'localhost';
+
+app.use(express.json());
+
+app.use('/api', mintRoutes)
 
 app.get('/', (req, res) => {
   res.send('Credence backend is running!');
